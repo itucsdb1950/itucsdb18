9485a25b321cf5023d9350898432062ebec33326
M. Fatih Y <mfy2333@protonmail.com>
2019-10-15 13:11:17 +0300

    Some silly modifications done to the returned string to test the deployment operation.

--- server.py
+++ server.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "Hello, world!"
+    return """Hello, world!
+              This is Klasse. 
+              Diese ist Klasse.
+              This team actually has nothing to do with German. The name "Klasse" is chosen because it's so klasse. :P"""
 
 
 if __name__ == "__main__":
