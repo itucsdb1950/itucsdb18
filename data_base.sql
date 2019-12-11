c01e3c006d40a9bd5ec8cb206ca648f597fa511a
kkarakamis <kutaykarakamis@hotmail.com>
2019-12-11 20:01:40 +0300

    Admin, location,Add CRN pages are added

--- data_base.sql
+++ data_base.sql
@@ -1,62 +1,104 @@
-CREATE TABLE PERSON (
-	id CHAR(9) PRIMARY KEY,
-	name VARCHAR(20),
-	age INTEGER,
-	username VARCHAR(10),
-	password VARCHAR(15)
-);
-CREATE INDEX person_name ON person(name);
-
-CREATE TABLE QUIZ (
-	quiz_numb INTEGER NOT NULL,
-	ques_numb INTEGER NOT NULL,
-	ques_text VARCHAR(250) NOT NULL,
-	right_ans VARCHAR(25) NOT NULL,
-	wrong_ans1 VARCHAR(25) NOT NULL,
-	wrong_ans2 VARCHAR(25),
-	wrong_ans3 VARCHAR(25),
-	PRIMARY KEY(quiz_numb,ques_numb)
-);
-
-
-CREATE TABLE ANSWERS (
-	quiz_numb INTEGER NOT NULL,
-	ques_numb INTEGER NOT NULL,
-	id CHAR(9),
-	given_ans VARCHAR(25)NOT NULL,
-	PRIMARY KEY (quiz_numb,ques_numb,id),
-	FOREIGN KEY (id) REFERENCES person(id)
-);
-
-ALTER TABLE PERSON
-ADD CHECK (age>=18); 
-
-
-CREATE TABLE CLASS (
-	crn INT(5),
-	course_code VARCHAR(7),
-	loc_id INT(5) FOREIGN KEY REFERENCES LOCATION(loc_id),
-	credit INT(1),
-
-);
-
-
-CREATE TABLE LOCATION (
-	class VARCHAR(10),
-	building CHAR(3),
-	day CHAR(3),
-	start_time TIME(),
-	end_time TIME(),
-	year YEAR,
-	loc_id INT(5) PRIMARY KEY
-);
-
-CREATE TABLE LOCATION (
-	dep_id INT(4),
-	dep_name VARCHAR(30),
-	fac_name VARCHAR(30),
-	dean_id INT FOREIGN KEY REFERENCES PERSON(id),
-	stu_delegate INT FOREIGN KEY REFERENCES PERSON(id)
+  --KUTAY KARAKAMIŞ
 	
-);
 
+	 CREATE TABLE PERSON (
+      id CHAR(9) PRIMARY KEY,
+      name VARCHAR(20),
+      age INTEGER,
+      username VARCHAR(10),
+      password VARCHAR(15)
+    );
+
+   ALTER TABLE PERSON ADD CHECK (age>=18);
+
+	-- CREATE INDEX person_name ON person(name)
+
+
+
+	 CREATE TABLE QUIZ (
+      quiz_numb INTEGER NOT NULL,
+      ques_numb INTEGER NOT NULL,
+      ques_text VARCHAR(250) NOT NULL,
+      right_ans VARCHAR(25) NOT NULL,
+      wrong_ans1 VARCHAR(25) NOT NULL,
+      wrong_ans2 VARCHAR(25),
+      wrong_ans3 VARCHAR(25),
+	        PRIMARY KEY(quiz_numb,ques_numb)
+	  );
+
+
+	 CREATE TABLE ANSWERS (
+      quiz_numb INTEGER NOT NULL,
+      ques_numb INTEGER NOT NULL,
+      id CHAR(9),
+      given_ans VARCHAR(25)NOT NULL,
+	    PRIMARY KEY (quiz_numb,ques_numb,id),
+	    FOREIGN KEY (id) REFERENCES person(id)
+	  );
+
+
+	
+	--ENES FURKAN ÖRNEK
+	CREATE TABLE LOCATION (
+     class VARCHAR(10),
+     building CHAR(3),
+     day CHAR(3),
+     capacity INT DEFAULT 80,
+     --start_time TIME,
+     --end_time TIME,
+     --year NUMERIC(4),
+     loc_id NUMERIC(5) SERIAL PRIMARY KEY
+
+  );
+
+	CREATE TABLE CLASS (
+     crn CHAR(5) PRIMARY KEY,
+     course_code VARCHAR(7),
+     loc_id NUMERIC(5) REFERENCES LOCATION(loc_id),
+     credit NUMERIC(1)
+  );
+
+
+	CREATE TABLE DEPARTMENT (
+    dep_id CHAR(4) PRIMARY KEY,
+    dep_name VARCHAR(30),
+    fac_name VARCHAR(30),
+    dean_id CHAR(9) REFERENCES PERSON(id),
+    stu_delegate CHAR(9) REFERENCES PERSON(id)
+  );
+
+
+	--MEHMET FATİH YILDIRIM
+	
+	CREATE TABLE STUDENT (
+		ID CHAR(9) PRIMARY KEY REFERENCES PERSON,
+		GPA NUMERIC(3, 2),
+		COMP_CREDITS INT NOT NULL DEFAULT 0,
+		dep_id CHAR(4) REFERENCES DEPARTMENT NOT NULL,
+		CHECK (COMP_CREDITS >= 0),
+		CHECK (GPA >= 0),
+		CHECK (GPA <= 4)
+	);
+	
+	
+	CREATE TABLE ENROLLMENT (
+		ID CHAR(9) REFERENCES PERSON NOT NULL,
+		CRN CHAR(5) REFERENCES CLASS NOT NULL,
+		ATTENDANCE INT,
+		CHECK (ATTENDANCE >= 0),
+		PRIMARY KEY (ID, CRN)
+	);
+	
+	
+	CREATE TABLE GRADES (
+		ID CHAR(9) REFERENCES PERSON NOT NULL,
+		CRN CHAR(5) REFERENCES CLASS NOT NULL,
+		TAKEN_FROM VARCHAR(15) NOT NULL,
+		PERCENTAGE INT NOT NULL,
+		GRADE INT NOT NULL,
+		CHECK (PERCENTAGE >= 0),
+		CHECK (PERCENTAGE <= 100),
+		CHECK (GRADE >= 0),
+		CHECK (GRADE <= 100),
+		PRIMARY KEY (ID, CRN, TAKEN_FROM)
+	);
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 from views import *
 
 app = Flask(__name__)
@@ -8,14 +8,27 @@ app = Flask(__name__)
 def home_page():
     return render_template("login.html")
 
+@app.route("/admin")
+def admin_page():
+    return render_template("base.html")
+
+@app.route("/admin/crn")
+def admin_crn_page():
+    return render_template("admin_crn.html")
+
+@app.route("/admin/location")
+def admin_location_page():
+    return render_template("admin_location.html")
+
 @app.route("/login", methods=['POST'])
 def login():
-	username = request.form.get('usrn')
-	password = request.form.get('pw')
-	user = check_user(username, password)
-	if user:
-		return render_template("home.html", record=user)
-	return "Your username and password is wrong"
+    username = request.form.get('usrn')
+    password = request.form.get('pw')
+    user = check_user(username, password)
+    if user:
+        return render_template("home.html", record=user)
+    return "Your username and password is wrong"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
