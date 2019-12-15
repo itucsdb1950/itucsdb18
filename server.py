5d537c630c93b8c6fe20dcc2c233d6a1ef3e8174
kkarakamis <kutaykarakamis@hotmail.com>
2019-12-15 15:36:07 +0300

    added some student pages in views and server

--- server.py
+++ server.py
@@ -124,5 +124,52 @@ def del_department(id):
     return redirect(url_for('admin_department_page'))
 
 
+@app.route("/student/<string:stu_num>")
+def student_page(stu_num):
+    student = views.get_student(stu_num)
+
+    # name, username, id, fac_name, gpa, comp_credits
+    name = student[1]
+    username = student[2]
+    faculty = student[4]
+    gpa = student[5]
+    compltd_crd = student[6]
+
+    return render_template("student.html", name=name, username=username, stu_num=stu_num,
+                           faculty=faculty, gpa=gpa, compltd_crd=compltd_crd)
+
+
+@app.route("/student/<string:stu_num>/courses")
+def student_courses_page(stu_num):
+    courses = views.get_courses(stu_num)
+    return render_template("student_courses.html", courses=courses)
+
+
+@app.route("/student/<string:stu_num>/attendance")
+def student_attendance_page(stu_num):
+    courses = views.get_attendance(stu_num)
+    return render_template("student_attendance.html", courses=courses)
+
+
+@app.route("/student/<string:stu_num>/grades")
+def student_grades_page(stu_num):
+    grades = views.get_grades(stu_num)
+
+    # split courses list with respect to crn.
+    # note that SQL query in views.get_grades() is written in a way that returned table is ordered by crn.
+    # therefore, only one iteration is sufficent
+    courses = []  # initialize list that will hold grades which are split by course
+    crn = grades[0].crn  # initilize crn with the first course's crn
+    i = 0
+    for grade in grades:  # iterate over the entire table
+        if grade.crn == crn:  # as long as crn remains same, add rows with that crn to courses[i]
+            courses[i].append(grade)
+        else:  # if another crn is reached
+            crn = grade.crn  # update crn to the new one
+            i += 1  # so that upcoming course's grades are written to the next index of courses
+
+    return render_template("student_grades.html", courses=courses)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
--- views_more.py
+++ views_more.py
@@ -1,68 +1,68 @@
-def get_student(stu_num):
-    statement = """
-                SELECT person.name, person.username, student.id, faculty.fac_name, student.gpa, student.comp_credits
-                    FROM student, person, faculty
-                    WHERE ( (person.id = student.id)
-                        AND (student.fac_id = faculty.id) ) 
-                    WHERE student.id = '{}'
-                """.format(stu_num)
-
-    with dbapi2.connect(db_url) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(statement)
-            record = cursor.fetchone()
-            return record
-
-
-def get_courses(stu_num):
-    statement = """
-                SELECT class.crn AS crn, class.course_code AS course_code, location.day AS day,
-                       location.building AS building, location.class AS class
-                    FROM student, enrollment, class, location
-                    WHERE ( (student.id = enrollment.student_id)
-                        AND (enrollment.crn = class.crn)
-                        AND (class.loc_id = location.id) )
-                    WHERE (student.id = '{}')
-                """.format(stu_num)
-
-    with dbapi2.connect(db_url) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(statement)
-            records = cursor.fetchall()
-            return records
-
-
-def get_attendance(stu_num):
-    statement = """
-                SELECT class.crn AS crn, class.course_code AS course_code,
-                       enrollment.attendance AS attendance
-                    FROM student, enrollment, class
-                    WHERE ( (student.id = enrollment.student_id)
-                        AND (enrollment.crn = class.crn) )
-                    WHERE (student.id = '{}')
-                """.format(stu_num)
-
-    with dbapi2.connect(db_url) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(statement)
-            records = cursor.fetchall()
-            return records
-
-
-def get_grades(stu_num):
-    statement = """
-                SELECT class.crn AS crn, class.course_code AS course_code, 
-                       grades.taken_from AS taken_from, grades.grade AS grade,
-                       grades.percentage AS percent, class.credits AS credits,
-                    FROM student, grades, class
-                    WHERE ( (student.id = grades.student_id)
-                        AND (grades.crn = class.crn) )
-                    WHERE (student.id = '{}')
-                    ORDER BY class.crn
-                """.format(stu_num)
-
-    with dbapi2.connect(db_url) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute(statement)
-            records = cursor.fetchall()
-            return records
+# def get_student(stu_num):
+#     statement = """
+#                 SELECT person.name, person.username, student.id, faculty.fac_name, student.gpa, student.comp_credits
+#                     FROM student, person, faculty
+#                     WHERE ( (person.id = student.id)
+#                         AND (student.fac_id = faculty.id) )
+#                     WHERE student.id = '{}'
+#                 """.format(stu_num)
+#
+#     with dbapi2.connect(db_url) as connection:
+#         with connection.cursor() as cursor:
+#             cursor.execute(statement)
+#             record = cursor.fetchone()
+#             return record
+#
+#
+# def get_courses(stu_num):
+#     statement = """
+#                 SELECT class.crn AS crn, class.course_code AS course_code, location.day AS day,
+#                        location.building AS building, location.class AS class
+#                     FROM student, enrollment, class, location
+#                     WHERE ( (student.id = enrollment.student_id)
+#                         AND (enrollment.crn = class.crn)
+#                         AND (class.loc_id = location.id) )
+#                     WHERE (student.id = '{}')
+#                 """.format(stu_num)
+#
+#     with dbapi2.connect(db_url) as connection:
+#         with connection.cursor() as cursor:
+#             cursor.execute(statement)
+#             records = cursor.fetchall()
+#             return records
+#
+#
+# def get_attendance(stu_num):
+#     statement = """
+#                 SELECT class.crn AS crn, class.course_code AS course_code,
+#                        enrollment.attendance AS attendance
+#                     FROM student, enrollment, class
+#                     WHERE ( (student.id = enrollment.student_id)
+#                         AND (enrollment.crn = class.crn) )
+#                     WHERE (student.id = '{}')
+#                 """.format(stu_num)
+#
+#     with dbapi2.connect(db_url) as connection:
+#         with connection.cursor() as cursor:
+#             cursor.execute(statement)
+#             records = cursor.fetchall()
+#             return records
+#
+#
+# def get_grades(stu_num):
+#     statement = """
+#                 SELECT class.crn AS crn, class.course_code AS course_code,
+#                        grades.taken_from AS taken_from, grades.grade AS grade,
+#                        grades.percentage AS percent, class.credits AS credits,
+#                     FROM student, grades, class
+#                     WHERE ( (student.id = grades.student_id)
+#                         AND (grades.crn = class.crn) )
+#                     WHERE (student.id = '{}')
+#                     ORDER BY class.crn
+#                 """.format(stu_num)
+#
+#     with dbapi2.connect(db_url) as connection:
+#         with connection.cursor() as cursor:
+#             cursor.execute(statement)
+#             records = cursor.fetchall()
+#             return records
