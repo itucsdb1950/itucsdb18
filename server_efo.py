9be8d303bf53597c89e593d7ed72cca17077cc04
sweat0198 <orneke16@itu.edu.tr>
2019-12-16 05:50:19 +0300

    meal page added

---
+++ server_efo.py
@@ -0,0 +1,218 @@
+from flask import Flask, render_template, request, redirect, url_for
+import views
+import server_more # this file may be merged with that file at the end
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    username = request.form.get('usrn')
+    password = request.form.get('pw')
+    user = views.check_user(username, password)
+    if user:
+        return render_template("base.html", record=user)
+    return "Your username and password is wrong"
+
+
+@app.route("/admin")
+def admin_page():
+    return render_template("base.html")
+
+
+@app.route("/admin/crn")
+def admin_crn_page():
+    locations = views.get_locations_for_crn()
+    crns = views.get_crns()
+
+    return render_template("admin_crn.html", locations=locations, crns=crns)
+
+
+@app.route("/admin/location")
+def admin_location_page():
+    locations = views.get_locations()
+
+    return render_template("admin_location.html", locations=locations)
+
+
+@app.route("/admin/persons")
+def admin_person_page():
+    person = views.get_person()
+
+    return render_template("admin_person.html", person=person)
+
+
+@app.route("/admin/food")
+def admin_food_page():
+    food = views.get_food()
+
+    return render_template("admin_food.html", food=food)
+
+
+@app.route("/admin/department")
+def admin_department_page():
+    person = views.get_person()
+    department = views.get_department()
+    info = views.get_dept_info()
+    return render_template("admin_department.html", person=person, department=department, info=info)
+
+
+@app.route("/add_crn", methods=['POST'])
+def add_crn():
+    crn = request.form.get('crn')
+    code = request.form.get('code')
+    loc_sel = request.form.get('loc_sel')
+    credits_sel = request.form.get('credits_sel')
+    if views.check_crn(crn, code, loc_sel):
+        views.add_crn(crn, code, loc_sel, credits_sel)
+    return redirect(url_for('admin_crn_page'))
+
+
+@app.route("/add_person", methods=['POST'])
+def add_person():
+    per_name = request.form.get('per_name')
+    per_num = request.form.get('per_num')
+    usern = request.form.get('usern')
+    passw = request.form.get('passw')
+    age = request.form.get('age')
+    type = request.form.get('type')
+    if views.check_person(usern):
+        added = views.add_person(per_name, per_num, usern, passw, age, type)
+        if added == 1:
+            # "User couldn't be added. Make sure all fields are convenient."
+            pass
+    return redirect(url_for('admin_person_page'))
+
+@app.route("/add_meal", methods=['POST'])
+def add_meal():
+    day = request.form.get('day_sel')
+    repast = request.form.get('repast')
+    soup = request.form.get('soup')
+    main = request.form.get('main')
+    side = request.form.get('side')
+    extras = request.form.get('extras')
+
+    if views.check_location(building, day, classroom):
+        views.add_location(building, day, classroom, capacity)
+    return redirect(url_for('admin_location_page'))
+
+
+
+
+
+@app.route("/add_location", methods=['POST'])
+def add_location():
+    building = request.form.get('building')
+    day = request.form.get('day_sel')
+    classroom = request.form.get('classroom')
+    capacity = request.form.get('capacity')
+    if views.check_location(building, day, classroom):
+        views.add_location(building, day, classroom, capacity)
+    return redirect(url_for('admin_location_page'))
+
+
+@app.route("/del_location/<int:loc_id>", methods=['GET'])
+def del_location(loc_id):
+    views.del_location(loc_id)
+    return redirect(url_for('admin_location_page'))
+
+
+@app.route("/add_department", methods=['POST'])
+def add_department():
+    dept = request.form.get('dept')
+    dean = request.form.get('dean')
+    delege = request.form.get('delege')
+    if views.check_department(dept):
+        views.add_department(dept, dean, delege)
+    return redirect(url_for('admin_department_page'))
+
+
+@app.route("/del_crn/<string:crn_num>", methods=['GET'])
+def del_crn(crn_num):
+    views.del_crn(crn_num)
+    return redirect(url_for('admin_crn_page'))
+
+
+
+@app.route("/del_person/<string:per_num>", methods=['GET'])
+def del_person(per_num):
+    views.del_person(per_num)
+    return redirect(url_for('admin_person_page'))
+
+
+@app.route("/del_department/<string:id>", methods=['GET'])
+def del_department(id):
+    views.del_department(id)
+    return redirect(url_for('admin_department_page'))
+
+
+@app.route("/add_food", methods=['POST'])
+def add_food():
+    dish_type = request.form.get('dish_type')
+    food_name = request.form.get('food_name')
+    food_cal = request.form.get('food_cal')
+    if views.check_food(dish_type, food_name):
+        views.add_food(dish_type, food_name, food_cal)
+    return redirect(url_for('admin_food_page'))
+
+
+@app.route("/del_food/<string:id>", methods=['GET'])
+def del_food(id):
+    views.del_food(id)
+    return redirect(url_for('admin_food_page'))
+
+
+@app.route("/student/<string:stu_num>")
+def student_page(stu_num):
+    student = views.get_student(stu_num)
+
+    # name, username, id, fac_name, gpa, comp_credits
+    name = student[1]
+    username = student[2]
+    faculty = student[4]
+    gpa = student[5]
+    compltd_crd = student[6]
+
+    return render_template("student.html", name=name, username=username, stu_num=stu_num,
+                           faculty=faculty, gpa=gpa, compltd_crd=compltd_crd)
+
+
+@app.route("/student/<string:stu_num>/courses")
+def student_courses_page(stu_num):
+    courses = views.get_courses(stu_num)
+    return render_template("student_courses.html", courses=courses)
+
+
+@app.route("/student/<string:stu_num>/attendance")
+def student_attendance_page(stu_num):
+    courses = views.get_attendance(stu_num)
+    return render_template("student_attendance.html", courses=courses)
+
+
+@app.route("/student/<string:stu_num>/grades")
+def student_grades_page(stu_num):
+    grades = views.get_grades(stu_num)
+
+    # split courses list with respect to crn.
+    # note that SQL query in views.get_grades() is written in a way that returned table is ordered by crn.
+    # therefore, only one iteration is sufficent
+    courses = []  # initialize list that will hold grades which are split by course
+    crn = grades[0].crn  # initilize crn with the first course's crn
+    i = 0
+    for grade in grades:  # iterate over the entire table
+        if grade.crn == crn:  # as long as crn remains same, add rows with that crn to courses[i]
+            courses[i].append(grade)
+        else:  # if another crn is reached
+            crn = grade.crn  # update crn to the new one
+            i += 1  # so that upcoming course's grades are written to the next index of courses
+
+    return render_template("student_grades.html", courses=courses)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
